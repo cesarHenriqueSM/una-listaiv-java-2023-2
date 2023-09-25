df32e04697cb3b7a7849c8e33fccb70bf8050545
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-24 23:34:16 -0300

    Update exercicio4.java

--- exercicio4.java
+++ exercicio4.java
@@ -7,24 +7,17 @@ public class exercicio4 {
 		
 		Scanner read = new Scanner(System.in);
 		
-			int i = 1;
-			float num1, num2, num3, num4, num5;
+			int soma = 0;
 		
-			System.out.printf("Informe o %d° número: ", i++);
-				num1 = read.nextFloat();
-			System.out.printf("Informe o %d° número: ", i++);
-				num2 = read.nextFloat();
-			System.out.printf("Informe o %d° número: ", i++);
-				num3 = read.nextFloat();
-			System.out.printf("Informe o %d° número: ", i++);
-				num4 = read.nextFloat();
-			System.out.printf("Informe o %d° número: ", i++);
-				num5 = read.nextFloat();
-				
-			float soma = num1 + num2 + num3 + num4 + num5;
+			for (int i = 1; i <= 5; i++) {
+				System.out.printf("Informe o %d° número: ", i);
+					int num = read.nextInt();
+				soma = soma + num;
+			}
+			
 			float media = soma/5;
 			
-			System.out.printf("A soma dos números informados é igual a %.1f, e a média equivale a %.2f.", soma, media);
+			System.out.printf("A soma dos números informados é igual a %d, e a média equivale a %.2f.", soma, media);
 
 	}
 }
